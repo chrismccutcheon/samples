c0e9a6d447dbe4a2600ce8e56652e500088c0785
mcheffin <cmccutcheon2015@gmail.com>
2015-08-26 17:07:12 -0700

    Added Python Spreadsheet nums

--- PythonProblems.py
+++ PythonProblems.py
@@ -19,11 +19,32 @@ def find_largest_diff(stocklist):
     print(maxval)
     print(maxdiff)
 
+#find_largest_diff(stocks)
 
-find_largest_diff(stocks)
+#permutation = ['a','b','c','d']
 
-permutation = ['a','b','c','d']
+#def permute_list(permutation, array):
+    #for i in range (len(array)):
 
-def permute_list(permutation, array):
-    for i in range (len(array)):
 
+# Take characters and convert them to numbers
+# A is 1 AA is 27  and ZZ is 702
+def spreadsheetnumbersystem(chars):
+    letterlist = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
+    sum = 0
+    if(len(chars) < 2):
+        for i in range (len(letterlist)):
+            if(chars[0] == letterlist[i]):
+                sum = i + 1
+    if(len(chars) >= 2):
+        for k in range (len(chars)-1, -1, -1):
+            for i in range (len(letterlist)):
+                if(chars[k] == letterlist[i]):
+                    if(k > 0):
+                        sum = 26 * (i + 1)
+                    if(k == 0):
+                        sum = sum + i + 1
+    print (sum)
+
+
+spreadsheetnumbersystem("ZZ")
